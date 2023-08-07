e665b1bc1b8f9f695f153fe30383caae14a03b16
pratikshamore02 <saritamadekar1998@gmail.com>
2023-08-07 16:24:18 +0530

    Reves string code done by sarita

--- src/main/java/Tutorial/Demo2.java
+++ src/main/java/Tutorial/Demo2.java
@@ -5,7 +5,7 @@ public class Demo2 {
 	public static void main(String[] args) {
 		String a="selenium";
 		for(int i=a.length()-1; i>=0; i--) {
-			System.out.println(a.charAt(i));
+			System.out.print(a.charAt(i));
 		}
 
 	}
