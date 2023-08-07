414c4cdfbeb57dbd7e868260c305c5ce924242d9
pratikshamore02 <saritamadekar1998@gmail.com>
2023-08-07 16:17:44 +0530

    string reverse code done by sarita

---
+++ src/main/java/Tutorial/Demo2.java
@@ -0,0 +1,13 @@
+package Tutorial;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		String a="selenium";
+		for(int i=a.length()-1; i>=0; i--) {
+			System.out.println(a.charAt(i));
+		}
+
+	}
+
+}
